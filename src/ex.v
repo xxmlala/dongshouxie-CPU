ff40022d2c963325e2e10283bb4fa3b65be6729c
xxmlala <1935570760@qq.com>
2020-07-01 14:18:32 +0800

    achieve the logic-compute and shift-compute

--- src/ex.v
+++ src/ex.v
@@ -16,7 +16,9 @@ module ex(
 );
 
 reg[`RegBus] logicout;
+reg[`RegBus] shiftres;
 
+//logic compute
 always @(*) begin
     if(rst == `RstEnable) begin
         logicout <= `ZeroWord;
@@ -24,12 +26,60 @@ always @(*) begin
         case(aluop_i)
             `EXE_OR_OP: begin
                 logicout <= reg1_i | reg2_i;
+            end
+            `EXE_AND_OP: begin
+                logicout <= reg1_i & reg2_i;
             end 
+            `EXE_NOR_OP: begin
+                logicout <= ~(reg1_i | reg2_i);
+            end
+            `EXE_XOR_OP: begin
+                logicout <= reg1_i ^ reg2_i;
+            end
             default: begin
                 logicout <= `ZeroWord;
             end
         endcase
     end
+end
+
+//shift position compute
+always @(*) begin
+    if(rst == `RstEnable) begin
+        shiftres <= `ZeroWord;
+    end else begin
+        case (aluop_i)
+            `EXE_SLL_OP: begin
+                shiftres <= reg2_i << reg1_i[4:0];
+            end
+            `EXE_SRL_OP: begin
+                shiftres <= reg2_i >> reg1_i[4:0];
+            end
+            `EXE_SRA_OP: begin
+                shiftres <= ({32{reg2_i[31]}} << (6'd32 - {1'b0, reg1_i[4:0]})) | reg2_i >> reg1_i[4:0];
+            end    
+            default: begin
+                shiftres <= `ZeroWord;
+            end
+        endcase
+    end
+end
+
+//choose the final compute res
+always @(*) begin
+    wd_o <= wd_i;
+    wreg_o <= wreg_i;
+    case (alusel_i)
+        `EXE_RES_LOGIC: begin
+            wdata_o <= logicout;
+        end
+        `EXE_RES_SHIFT: begin
+            wdata_o <= shiftres;
+        end
+        default: begin
+            wdata_o <= `ZeroWord;
+        end
+    endcase
 end
 
 always @(*) begin
